2a53c0c1a77d74fc9fd8ff3e9d6742337bbd358c
Hailing Fang <benjaminfang.ol@outlook.com>
2024-04-11 11:04:57 +0800

    add a class to parse annoread data

--- countmtx.py
+++ countmtx.py
@@ -5,6 +5,102 @@ Read in annread output, and creat count matrix file.
 
 import argparse
 
+class ANNOTREAD_DATE:
+    """
+    The class to handle reads annotation data generate by annoread.py.
+    """
+    def __init__(self, anno_file):
+        """
+        Structure Annotation File
+
+        Parameters
+        ---------------
+        anno_file: The result file generate by annoread.py
+
+        Returns
+        ------------
+        dt_out: Structured data of annoread_file.
+            {gene_name: 
+                {gene_id: 
+                    {
+                        'ref_id': string,
+                        'gene_range': (left, right),
+                        'ori': string,
+                        'transcipts': {
+                            transcipt_id: {
+                                'gbkey': string,
+                                'trans_range': list,
+                                'start_codon': list,
+                                'stop_codon': list,
+                                'reads': [
+                                    [read_id, read_pos, read_len, alig_pos],
+                                    ...
+                                ]
+                            }
+                        }  
+
+                    }
+                }
+            }
+        """
+        self.__data = None
+        data = {}
+        gene_name = None
+        gene_id = None
+        transcipt_id = None
+        fin = open(anno_file, "r")
+        for line in fin:
+            line = line.rstrip()
+            if line[0] == ">":
+                gene_name = line[1:]
+                data[gene_name] = {}
+            elif line[0] == "&":
+                line = line[1:].split('\t')
+                gene_id, ref_id, gene_range, ori = line
+                gene_range = [int(ele) for ele in gene_range.split(",")]
+                data[gene_name][gene_id] = {}
+                data[gene_name][gene_id]["ref_id"] = ref_id
+                data[gene_name][gene_id]["gene_range"] = gene_range
+                data[gene_name][gene_id]["ori"] = ori
+                data[gene_name][gene_id]["transcripts"] = {}
+            elif line[0] == "$":
+                line = line[1:].split('\t')
+                transcipt_id, gbkey, trans_range, start_codon, stop_codon = line
+                trans_range = [ele.split(",") for ele in trans_range.split(";")]
+                trans_range = [(int(ele[0]), int(ele[1])) for ele in trans_range]
+                if start_codon != "*":
+                    start_codon = [ele.split(",") for ele in start_codon.split(";")]
+                    start_codon = [(int(ele[0]), int(ele[1])) for ele in start_codon]
+                else:
+                    start_codon = None
+                if stop_codon != "*":
+                    stop_codon = [ele.split(",") for ele in stop_codon.split(";")]
+                    stop_codon = [(int(ele[0]), int(ele[1])) for ele in stop_codon]
+                else:
+                    stop_codon = None
+                data[gene_name][gene_id]["transcripts"][transcipt_id] = {}
+                data[gene_name][gene_id]["transcripts"][transcipt_id]["gbkey"] = gbkey
+                data[gene_name][gene_id]["transcripts"][transcipt_id]["trans_range"] = trans_range
+                data[gene_name][gene_id]["transcripts"][transcipt_id]["start_codon"] = start_codon
+                data[gene_name][gene_id]["transcripts"][transcipt_id]["stop_codon"] = stop_codon
+                data[gene_name][gene_id]["transcripts"][transcipt_id]["reads"] = []
+            else:
+                line = line.split('\t')
+                read_id, read_pos, read_len, align_pos = line
+                read_pos = int(read_pos)
+                read_len = int(read_len)
+                align_pos = [ele.split(",") for ele in align_pos.split(";")]
+                align_pos = [(int(ele[0]), int(ele[1])) for ele in align_pos]
+                data[gene_name][gene_id]["transcripts"][transcipt_id]["reads"].append([read_id, read_pos, read_len, align_pos])
+        fin.close()
+        self.__data = data
+
+
+    def get_data(self):
+        return self.__data
+
+
+
 
 def getargs():
     parser = argparse.ArgumentParser()
@@ -14,27 +110,8 @@ def getargs():
     return args.annoread, args.out
 
 
-def struc_annoread(annoread_file):
-    """
-    Structure Annotation File
-
-    Parameters
-    ---------------
-    annoread_file: The result file generate by annoread.py
-
-    Returns
-    ------------
-    dt_out: Structured data of annoread_file.
-    """
-    
-    dt_out = {}
-    fin = open(annoread_file, "r")
-
-    fin.close()
-
-
 if __name__ == "__main__":
     anno_files, out = getargs()
 
     for annofile in anno_files:
-        pass
+        annodata = ANNOTREAD_DATE(annofile)
