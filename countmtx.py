f4e7eef6efebbb3160cfd45632c4e0b7e61216d2
Hailing Fang <benjaminfang.ol@outlook.com>
2024-04-11 09:34:56 +0800

    revise counmtx

--- countmtx.py
+++ countmtx.py
@@ -4,7 +4,7 @@ Read in annread output, and creat count matrix file.
 """
 
 import argparse
-import os
+
 
 def getargs():
     parser = argparse.ArgumentParser()
@@ -15,75 +15,26 @@ def getargs():
 
 
 def struc_annoread(annoread_file):
-    read_gene_dic = {}
+    """
+    Structure Annotation File
+
+    Parameters
+    ---------------
+    annoread_file: The result file generate by annoread.py
+
+    Returns
+    ------------
+    dt_out: Structured data of annoread_file.
+    """
+    
+    dt_out = {}
     fin = open(annoread_file, "r")
-    for line in fin:
-        line = line.rstrip().split("\t")
-        readid, gene = line[0], line[-1]
-        if readid not in read_gene_dic:
-            read_gene_dic[readid] = set([gene])
-        else:
-            read_gene_dic[readid].add(gene)
+
     fin.close()
-    return read_gene_dic
 
-
-def filter_read(read_gene_dic):
-    data_out = {}
-    uniq_gene_read = 0
-    mult_gene_read = 0
-    for read in read_gene_dic:
-        if len(read_gene_dic[read]) == 1:
-            data_out[read] = read_gene_dic[read].pop()
-            uniq_gene_read += 1
-        else:
-            mult_gene_read += 1
-    print(f"read mapped to one gene: {uniq_gene_read}")
-    print(f"read mapped to multiple genes: {mult_gene_read}")
-    return data_out
-
-
-def count_genes_read(read_gene_dic):
-    gene_read_count = {}
-    for read, gene in read_gene_dic.items():
-        if gene not in gene_read_count:
-            gene_read_count[gene] = 1
-        else:
-            gene_read_count[gene] += 1
-
-    return gene_read_count
-
-
-def main():
-    annoread_files, out = getargs()
-    count_dics = []
-    for anno_file in annoread_files:
-        print(f"Processing {anno_file}")
-        read_gene_dic = struc_annoread(anno_file)
-        read_gene_dic = filter_read(read_gene_dic)
-        gene_count = count_genes_read(read_gene_dic)
-        count_dics.append(gene_count)
-    
-    fout = open(out, "w")
-    headline = "\t".join(["geneid"] + ["_".join(".".join(os.path.basename(ff).split(".")[:-1]).split('-')) for ff in annoread_files])
-    print(headline, file=fout)
-    gene_union = set()
-    for dic in count_dics:
-        gene_union |= set(dic.keys())
-    gene_union = list(gene_union)
-    gene_union.sort()
-    for gene in gene_union:
-        counts = []
-        for dic in count_dics:
-            count_gene = dic.get(gene)
-            if count_gene:
-                counts.append(str(count_gene))
-            else:
-                counts.append(str(0))
-        print("\t".join([gene] + counts), file=fout)
-    fout.close()
-    return 0
-    
 
 if __name__ == "__main__":
-    main()
+    anno_files, out = getargs()
+
+    for annofile in anno_files:
+        pass
